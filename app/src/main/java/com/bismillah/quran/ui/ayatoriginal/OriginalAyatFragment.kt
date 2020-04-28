d6f7adbf4f325017e5354c402731ce6cbc7d43c5
kalabaev <kalabaevshj@gmail.com>
2020-04-28 19:05:17 +0500

    bag fixed

--- app/src/main/java/com/bismillah/quran/ui/ayatoriginal/OriginalAyatFragment.kt
+++ app/src/main/java/com/bismillah/quran/ui/ayatoriginal/OriginalAyatFragment.kt
@@ -2,7 +2,6 @@ package com.bismillah.quran.ui.ayatoriginal
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.lifecycle.Observer
@@ -10,15 +9,16 @@ import androidx.navigation.fragment.navArgs
 import com.bismillah.quran.R
 import com.bismillah.quran.Settings
 import com.bismillah.quran.ui.base.BaseFragment
-import kotlinx.android.synthetic.main.fragment_explanation_ayat.*
+import kotlinx.android.synthetic.main.fragment_ayat_original.*
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class OriginalAyatFragment : BaseFragment(R.layout.fragment_explanation_ayat) {
+class OriginalAyatFragment : BaseFragment(R.layout.fragment_ayat_original) {
 
     private val viewModel : OriginalAyatViewModel by viewModel()
     private val settings: Settings by inject()
     private val safeArgs: OriginalAyatFragmentArgs by navArgs()
+    private val adapter: OriginalAyatRVAdapter by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,14 +27,11 @@ class OriginalAyatFragment : BaseFragment(R.layout.fragment_explanation_ayat) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        rvOriginalAyatList.adapter = adapter
         viewModel.getOriginalAyatListBySureId(safeArgs.sureId)
         viewModel.getSureById(safeArgs.sureId)
         viewModel.originalAyatList.observe(viewLifecycleOwner, Observer {
-            var ayatText = ""
-            for (ayat in it) {
-                ayatText += "${ayat.text}\n\n"
-            }
-            tvExplanationText.text = ayatText
+            adapter.models = it
         })
         viewModel.currentSure.observe(viewLifecycleOwner, Observer {
             tvToolbarTitle.text = it.originalName
@@ -43,13 +40,13 @@ class OriginalAyatFragment : BaseFragment(R.layout.fragment_explanation_ayat) {
         btnMinus.setOnClickListener {
             val currentTextSize = settings.getArabTextSize()
             settings.setArabTextSize(currentTextSize-1)
-            tvExplanationText.textSize = (currentTextSize-1).toFloat()
+            adapter.update()
         }
 
         btnPlus.setOnClickListener {
             val currentTextSize = settings.getArabTextSize()
             settings.setArabTextSize(currentTextSize+1)
-            tvExplanationText.textSize = (currentTextSize + 1).toFloat()
+            adapter.update()
         }
 
         backButton.setOnClickListener {
---
+++ app/src/main/java/com/bismillah/quran/ui/ayatoriginal/OriginalAyatViewHolder.kt
@@ -0,0 +1,14 @@
+package com.bismillah.quran.ui.ayatoriginal
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bismillah.quran.Settings
+import com.bismillah.quran.data.model.QuranText
+import kotlinx.android.synthetic.main.item_ayat.view.*
+
+class OriginalAyatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun populateModel(ayat: QuranText, settings: Settings) {
+        itemView.tvText.text = ayat.text
+        itemView.tvText.textSize = settings.getArabTextSize().toFloat()
+    }
+}
