16ffe813e152a9dfb68a310ebc5d367f989c241f
kalabaev <kalabaevshj@gmail.com>
2020-05-05 12:49:55 +0500

    adding new sections

--- app/src/main/java/com/bismillah/quran/ui/ayatoriginal/OriginalAyatViewModel.kt
+++ app/src/main/java/com/bismillah/quran/ui/ayatoriginal/OriginalAyatViewModel.kt
@@ -1,6 +1,5 @@
 package com.bismillah.quran.ui.ayatoriginal
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
