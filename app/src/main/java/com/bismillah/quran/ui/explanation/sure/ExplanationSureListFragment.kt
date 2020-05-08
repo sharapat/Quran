a3db09dfaf45f94a5fb175ceaeab9bffcf6582ae
kalabaev <kalabaevshj@gmail.com>
2020-05-08 21:28:21 +0500

    processing

--- app/src/main/java/com/bismillah/quran/ui/explanation/sure/ExplanationSureListFragment.kt
+++ app/src/main/java/com/bismillah/quran/ui/explanation/sure/ExplanationSureListFragment.kt
@@ -47,9 +47,7 @@ class ExplanationSureListFragment : BaseFragment(R.layout.fragment_sure_list) {
         }
 
         etSearch.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                fillRecyclerView(s)
-            }
+            override fun afterTextChanged(s: Editable?) { fillRecyclerView(s) }
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         })
